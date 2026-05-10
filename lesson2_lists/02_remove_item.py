afee7d83a26c95886fce328c2842f3c2d9409ec5
vijnaicker <vijhannaicker@Vijhans-MacBook-Air.local>
2026-05-10 01:18:10 -0700

    removelistitembyindex

---
+++ lesson2_lists/02_remove_item.py
@@ -0,0 +1,20 @@
+numbers = [10, 20, 30]
+
+print(numbers)
+print()
+
+# remove an item from a list
+# Remove using the value
+numbers.remove(10)
+print(numbers)
+
+# Remove using the index
+# Example
+numbers_two_example = [32, 3,25,45]
+print(type(numbers_two_example))
+print(numbers_two_example[3])
+print(numbers_two_example[1])
+numbers_two_example.remove(45)
+#print(numbers_two_example[3])
+print(numbers_two_example)
+#numbers_two_example.remove[0]
