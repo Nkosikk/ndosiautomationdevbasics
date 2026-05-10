702fee156170ac79ce1a87d481efc1c211464367
rilinde13 <rilinde41@gmail.com>
2026-05-10 19:09:01 +0200

    Added code to simple list file

---
+++ AutomationProjects/lesson2/list/01_simple_list.py
@@ -0,0 +1,29 @@
+numbers = [10, 20, 30]
+# index starts at 0, to access each item we use an index
+
+# numbers = [10, 20, 'string']
+# Bad programming mixing data types in one list
+
+print()
+
+#See all numbers
+print(numbers)
+print(type(numbers))
+# to print out data type entered for validation
+
+
+print()
+
+# print(type(10))
+print(type(numbers[0]))
+
+print()
+
+#Access index 0
+print(numbers[0])
+print(numbers[1])
+print(numbers[2]) # reference to line 4
+
+print()
+
+print(numbers[100]) # Index Error
