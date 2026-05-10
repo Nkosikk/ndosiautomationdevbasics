e66dc5d08b644e644b7af76b2b47d2b299f6da2f
Gift Ndosi Automation <gcmakofane@gmail.com>
2026-05-10 22:50:38 +0200

    List in Pythod examples

--- lesson_practice/list/05_add_item.py
+++ lesson_practice/list/05_add_item.py
@@ -1,7 +1,4 @@
-numbers  = [10, 20, 30] #access the numbers items using index
-print(f'number item at index 0 is: {numbers}')
-print(f'Number at index 0 is, {numbers[0]}')
-print(f'Number at index 1 is, {numbers[1]}')
-print(f'Number at index 2 is, {numbers[2]}')
-print(type(numbers))
-print(numbers[100]) #IndexError
+numbers  = [10, 20, 30] #Add items
+numbers.append(40)
+print(numbers)
+
