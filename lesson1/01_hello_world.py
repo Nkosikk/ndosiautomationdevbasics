49499603b7b940bb6857f6cd039937e15ce9140b
rilinde13 <rilinde41@gmail.com>
2026-05-10 19:06:05 +0200

    clean repo

--- lesson1/01_hello_world.py
+++
@@ -1,4 +0,0 @@
-print('Hello world')
-
-# hello 123
-print("Hello world 2")
--- lesson1/02_variables_and_data_types.py
+++
@@ -1,5 +0,0 @@
-# string, int, float, boolean
-my_string = 'Mary'
-print(my_string)
-print(type(my_string))
-print()   # empty line
