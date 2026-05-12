ed69f3b9a4865f18522012837fff867b23c0cc76
Raaga Krishna <vraagakrishna@gmail.com>
2026-05-12 19:46:44 +0200

    Loops in Python

---
+++ lesson3/loops/02_bad_variable_name.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30]
+name = 'Mary'
+
+for name in numbers:
+    print(name)
+
+print(name)
---
+++ lesson3/loops/11_execute_code_at_end_of_while_loop.py
@@ -0,0 +1,9 @@
+number = 1
+
+while number < 10:
+    print(f'Forever {number}')
+
+    number = number + 1
+
+else:  # execute after the normal loop
+    print('Done looping')
