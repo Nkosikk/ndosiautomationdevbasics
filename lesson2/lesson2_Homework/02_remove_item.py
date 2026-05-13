707f90dcf261529d2ec7ce37acb3b346ae396ae3
ysobekwa <yusobekwa@gmail.com>
2026-05-14 00:04:54 +0200

    added new numbers to the list & removed index 3

--- lesson2/lesson2_Homework/02_remove_item.py
+++ lesson2/lesson2_Homework/02_remove_item.py
@@ -1,10 +1,12 @@
-numbers = [50, 60, 70]
+numbers = [50, 60, 70, 80 , 90]
+print(numbers)
+numbers = [50, 60, 70, 80, 90]
 
 print(numbers)
 print()
 
  #remove item using index
 
-numbers.pop(1)
+numbers.pop(3)
 print(numbers)
 print()
