6a4f14c8f071d2b968462ca4cc96d2bb9be42fe9
ysobekwa <yusobekwa@gmail.com>
2026-05-14 08:30:17 +0200

    delete items & add new item

--- lesson2/lesson2_Homework/08_remove_item_using_del.py
+++ lesson2/lesson2_Homework/08_remove_item_using_del.py
@@ -2,10 +2,10 @@ numbers=[1,2,3,4,5,6,7,8,9]
 print(numbers)
 print()
 
-del numbers[8]
+del numbers[0]
 print(numbers)
 print()
 
-numbers.insert(0,150)
+numbers.insert(0,1000)
 print(numbers)
 print()
