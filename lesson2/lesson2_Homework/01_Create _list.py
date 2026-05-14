47d8980a14dce78388ec907ad22b6c0a4bdf0fc5
ysobekwa <yusobekwa@gmail.com>
2026-05-14 08:48:01 +0200

    I have just added a list

--- lesson2/lesson2_Homework/01_Create _list.py
+++ lesson2/lesson2_Homework/01_Create _list.py
@@ -1,13 +1,9 @@
-numbers = [50, 60, 70]
-print(numbers)
-print(type(numbers))
-
-#add a new items to the list
-
 numbers = [50, 60, 70,80,90,100]
 print(numbers)
 print(type(numbers))
-print()
+
+
+
 
 
 
