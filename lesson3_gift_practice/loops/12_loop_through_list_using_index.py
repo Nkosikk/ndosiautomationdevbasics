db194f713dab5f9ab9acaf51c920d5e7d02c42e9
Gift Ndosi Automation <gcmakofane@gmail.com>
2026-05-15 15:10:43 +0200

    Updated loop

--- lesson3_gift_practice/loops/12_loop_through_list_using_index.py
+++ lesson3_gift_practice/loops/12_loop_through_list_using_index.py
@@ -3,3 +3,18 @@ numbers = [10, 20, 30, 40, 50]
 for index in range(2, len(numbers)):
      print(f'index -> {index}, value -> {numbers[index]}')
 
+print('------------------')
+
+users = [
+     {
+          'name': 'Mary'
+     },
+     {
+          'name': 'Peter'
+     }
+]
+
+for user in users:
+     print(user)
+     print(f'You are {user['name']}')
+
