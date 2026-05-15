8766087fd9979961ea133e2a03c79dfce56377a4
Gift Ndosi Automation <gcmakofane@gmail.com>
2026-05-15 14:00:27 +0200

    Loops in python

---
+++ lesson3_gift_practice/loops/10_execute_code_at_end_of_loop.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30]
+
+for number in numbers:
+    print(number)
+else:
+    print('Done looping')
+
---
+++ lesson3_gift_practice/loops/12_loop_through_list_using_index.py
@@ -0,0 +1,5 @@
+numbers = [10, 20, 30, 40, 50]
+
+for index in range(2, len(numbers)):
+     print(f'index -> {index}, value -> {numbers[index]}')
+
