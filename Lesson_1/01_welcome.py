d74aade77dd1323092492d72ea32faf8f08cc6ea
Ntakuse <ntaku86@gmail.com>
2026-05-18 20:29:31 +0200

    Homework 7 May

---
+++ Lesson_1/01_welcome.py
@@ -0,0 +1,1 @@
+print ('Hello\nNtakuseni\nTshindane\nWelcome')
---
+++ Lesson_1/Homework_05 May.py
@@ -0,0 +1,25 @@
+#Person 1
+name1 = input("Enter name1:")
+age1 = int(input("Enter age1:"))
+height1 = float(input("Enter height1:"))
+
+
+
+
+#Person 2
+name2 = input("Enter name2:")
+age2 = int(input("Enter age2:"))
+height2 = float(input("Enter height2:"))
+
+
+
+#Calculate total height
+total_height = height1 + height2
+
+#print user Person1 information
+print(f"Hello,your name is {name1}! You are {age1} years old, your height is {height1}cm.")
+
+print(f"Hello,your name is {name2}! You are {age2} years old, your height is {height2}cm.")
+
+print(f"Total height is {total_height} cm.")
+
