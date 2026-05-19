17a9269f4c95fc49b08701c51220208f0064110e
Nthabza <nthamot80@gmail.com>
2026-05-19 10:49:40 +0200

    create list, added, removed, updated count on a list items

---
+++ My Lesson2 task/Lists/Add item from the list.py
@@ -0,0 +1,4 @@
+number=[12, 24, 36, 48]
+number.append(60)
+number.append(72)
+print(number)
---
+++ My Lesson2 task/Lists/My list Indexing.py
@@ -0,0 +1,7 @@
+number=[12, 24, 36, 48]
+#List indexing
+
+print (number[0])
+print (number[1])
+print (number[2])
+print (number[3])
