f34d47eac3331617c6a34ce0426bf0fdf1d8e032
mchauke250 <82161774+mchauke250@users.noreply.github.com>
2026-05-20 11:26:58 +0200

    Updated to allow the user to capture the student grade

--- lesson3/conditional_statements/03_else_if.py
+++ lesson3/conditional_statements/03_else_if.py
@@ -1,5 +1,7 @@
 # student_grade = int(input('Enter your grade'))   # can ask the user to input data
-student_grade = 50
+#student_grade = 50
+
+student_grade = int(input('Enter the student grade: '))
 
 if student_grade <= 59:
     print('E')
