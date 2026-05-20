746c866170da10a13acdf888b0946efae8fe8380
mchauke250 <82161774+mchauke250@users.noreply.github.com>
2026-05-20 11:16:21 +0200

    Conditional Statements

---
+++ lesson3/conditional_statements/01_simple_conditional_statement.py
@@ -0,0 +1,6 @@
+temperature = 20
+
+if temperature > 25:
+    print('Hot')
+
+print('Very Hot')  # It has to be indented for it be work ONLY if the condition is met
---
+++ lesson3/conditional_statements/03_else_if.py
@@ -0,0 +1,15 @@
+# student_grade = int(input('Enter your grade'))   # can ask the user to input data
+student_grade = 50
+
+if student_grade <= 59:
+    print('E')
+elif student_grade <= 69:
+    print('D')
+elif student_grade <= 79:
+    print('C')
+elif student_grade <= 89:
+    print('B')
+else:
+    print('A')
+
+
