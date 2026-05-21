dd5a4de404d917b67fd07afe73a035ce4e2ab77c
Nokuzola1996 <45654921+Nokuzola1996@users.noreply.github.com>
2026-05-21 20:02:18 +0200

    Add files via upload

---
+++ 02_variables_and_data_types.py
@@ -0,0 +1,5 @@
+# string, int, float, boolean
+my_string = 'Noks'
+print(my_string)
+print(type(my_string))
+print()
---
+++ 03_integer_data_type.py
@@ -0,0 +1,19 @@
+my_int = 30
+print(my_int)
+print(type(my_int))
+print()
+
+my_float = 50.56
+print(my_float)
+print(type(my_float))
+print()
+
+my_bool = True
+my_bool_2 = False
+print(my_bool)
+print(type(my_bool))
+print()
+
+print(my_bool_2)
+print(type(my_bool_2))
+print()
