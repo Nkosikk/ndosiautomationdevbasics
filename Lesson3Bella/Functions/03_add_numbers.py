25997a3e387af11e96227a96bd6e6cb3d03dcc2c
Bella <jacobsbulelwa61@gmail.com>
2026-05-24 20:45:40 +0200

    functions

---
+++ Lesson3Bella/Functions/03_add_numbers.py
@@ -0,0 +1,9 @@
+def add(num1: int, num2: int):  #type hints
+    return num1 + num2
+
+sum_of_numbers = add(10, 10)
+print(sum_of_numbers)
+
+sum_of_numbers = add(10, 20)
+print(sum_of_numbers)
+
---
+++ Lesson3Bella/Functions/04_function_with_loop.py
@@ -0,0 +1,9 @@
+def print_even_numbers(num_list: list):
+    print(num_list)
+    print(len(num_list))
+    # TODO: finish this off...
+
+
+numbers = [10, 20, 30, 34, 54, 31]
+print_even_numbers(numbers)
+
