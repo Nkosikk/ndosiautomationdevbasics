e1b4ba2d07ed905b4a2876b8110d43b0b6d276ed
Nthabza80 <nthamot80@gmail.com>
2026-05-29 07:37:42 +0200

    Clarify continue statement comment in loop corrected

--- Nthabza_Lesson3/Simple Loops/skip_more_than_one_range_for_loop.py
+++ Nthabza_Lesson3/Simple Loops/skip_more_than_one_range_for_loop.py
@@ -2,6 +2,6 @@ name = ["Peter", "John", "Abiel","Sipho","Mpho"]
 for name in name:
 
     if name == "John" or name== "Sipho":
-        continue # skip the loop when the number is at 88
+        continue # skip the loop when the name is John or name Sipho
 
     print(name)
