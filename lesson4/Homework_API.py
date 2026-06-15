209213e4c91c3b2e2832a039b176fd37a4aaeab9
Ennie Maneke <enniemaneke@gmail.com>
2026-06-16 00:53:36 +0200

    lesson4 Homework

---
+++ lesson4/Homework_API.py
@@ -0,0 +1,44 @@
+import requests
+
+response = requests.get('https://www.ndosiautomation.co.za/APIDEV/groups')
+print(response)
+
+print('')
+print('Response from API')
+print(response.json())
+
+print()
+print(f'Status code is {response.status_code}')
+
+print()
+
+print('')
+print('Response from API')
+groups_response = response.json()   # Get output from the API response
+print(groups_response)
+
+
+print()
+print(f'Status code is {response.status_code}')   # Status code of API response
+
+
+
+print()
+print()
+print()
+
+
+# Access data from the API response
+print(groups_response["success"])
+print(groups_response["data"])
+print(type(groups_response["data"]))   # 'data' is a list
+
+
+print()
+print()
+
+my_list = groups_response["data"]
+
+for group in my_list:
+    # print(group)
+    print(f'Group Id is {group["Id"]} and Name is {group["Name"]}')
